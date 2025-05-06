637772abc741f309d46dfddcebd6aad571ba33ac
diegocoronado1412 <124116182+diegocoronado1412@users.noreply.github.com>
2025-05-05 19:01:41 -0500

    Delete TravelsColombia/TravelsColombia directory

--- TravelsColombia/TravelsColombia/app/src/main/java/com/example/travelscolombia/BottomNavItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.travelscolombia.ui.navigation
-
-import androidx.annotation.DrawableRes
-
-data class BottomNavItem(
-    val route: String,
-    val label: String,
-    @DrawableRes val icon: Int
-)
